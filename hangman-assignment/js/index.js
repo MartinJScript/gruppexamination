cf7cf15b3a83975de766eb0c5ac7dbaec4f17bd2
MartinJScript <martin88@telia.com>
2022-10-31 21:06:56 +0100

    basfunktioner
    Basfunktioner fungerar nu

--- hangman-assignment/js/index.js
+++ hangman-assignment/js/index.js
@@ -1,9 +1,73 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
+parts = document.querySelector('figure');
+wordSpace = document.getElementById("word");
+btn = document.querySelector("button");
 
- */
+const usePart = ["scaffold", "head", "body", "arms", "legs"];
+const words = ["banana", "orange", "kiwi", "lemon", "pear"];
+
+function Wrong() {
+   parts.classList.add(usePart[0]);
+   usePart.shift();
+ 
+   if (usePart[0] == undefined) {
+    wordSpace.innerHTML = "<h1>YOU LOST!<h1>";
+   }
+}
+
+ let correctWord = "";
+ let guessedLetter = [];
+
+function getText(){
+  let inputField = document.getElementById("textInput").value;
+  return inputField;
+}
+
+ function generateWord() {
+   
+   let number = Math.floor(Math.random() * words.length);
+   for (let index = 0; index < words[number].length; index++) {
+       guessedLetter.push("*");  
+   }
+   console.log(guessedLetter);
+   for (let index = 0; index < guessedLetter.length; index++) {
+       correctWord += guessedLetter[index];
+   }
+   
+   wordSpace.innerHTML = `
+   <h1>${correctWord}</h1>`;
+   
+   return words[number];
+ }
+let blala = btn.addEventListener("click", gameEngine)
+randomWord = generateWord();
+
+function gameEngine() {
+  guess = getText();
+  guessArray = [...randomWord];
+    guessIndex =  guessArray.indexOf(guess);
+
+    if (guess.length == 1) {
+      if (guessIndex == -1) {
+        Wrong()
+      }
+          for (let index = 0; index < randomWord.length; index++) {        
+            if (guess == randomWord[index]) {
+              guessedLetter[index] = guess;
+              correctWord = "";
+              for (let index = 0; index < guessedLetter.length; index++) {
+                correctWord += guessedLetter[index];
+              }
+              wordSpace.innerHTML = `
+              <h1>${correctWord}</h1>`;
+              won = guessedLetter.indexOf("*");
+              if (won == -1) {
+                wordSpace.innerHTML = "<h1>YOU WON!<h1>";
+              }
+              
+            }       
+          }
+    }
+    else {
+      console.log("bad boy")
+    }
+}
